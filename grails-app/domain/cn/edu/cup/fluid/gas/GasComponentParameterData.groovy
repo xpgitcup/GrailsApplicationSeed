28fe02b868b78a39ee91d2c834061ce28ee6c0cf
希望的石头CUP <xplixina@sina.com>
2016-01-05 11:23:26 +0800

    3.1B --

--- grails-app/domain/cn/edu/cup/fluid/gas/GasComponentParameterData.groovy
+++ grails-app/domain/cn/edu/cup/fluid/gas/GasComponentParameterData.groovy
@@ -2,11 +2,21 @@ package cn.edu.cup.fluid.gas
 
 class GasComponentParameterData {
     
+    String name
     Double value 
+    String unit
     
     static belongsTo = [gasComponent: GasComponent, gasComponentParameter: GasComponentParameter]
 
     static constraints = {
+        name(nullable: true)
+        value()
+        unit(nullable: true)
+    }
+    
+    static mapping = {
+        sort: 'id'
+        sort: 'name'
     }
     
     String toString() {
